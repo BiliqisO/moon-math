5d1653708ecea702f4646bc535cd629ac8aeb042
Biliqis Onikoyi <105673949+BiliqisO@users.noreply.github.com>
2024-10-14 08:28:11 +0100

    init perseden's hash function

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,70 @@
 fn main() {
     println!("Hello, world!"); 
-    let ans =remainder_class_algorithm(2,6, String::from("*") ,5);
+    let ans =persedens_hash_function(5);
 
-      println!("gcd {:?}", ans);
+      println!("ans {:?}", ans);
       
 }
-enum Action {
-  Addition,
-  Multiplication,
-  None
+fn persedens_hash_function( mod_num:u32 ) -> Option<Vec<u32>>  {
+let mut generators :Vec<u32>= vec![];
+let mut generator: u32 = 1;
+let mut exponent = 0;
+
+while  generator < mod_num {
+  generator += 1;
+  while  exponent < mod_num{
+    exponent+=1;
+   println!("mod_num {:?}", mod_num);
+   println!("exponent {:?}", exponent);
+  println!("generator {:?}", generator);
+  let result = u32::pow(generator, exponent) % mod_num;
+  // if  {
+  //   return None;
+  // }
+  if result  == 1  {     
+    generators.push(generator);
+   println!("generators {:?}", generators);
+} else if result  == 1 && exponent < mod_num{
+  return None;
+};
+  }
+
+ 
+
+
+   
 }
-fn remainder_class_algorithm(a:i64,b:i64, operator:String, mod_num:i64 ) -> i64  {
-let mut final_ans = 0;
-let mut  inter_action:Action = Action::None;
-
-  match operator.as_str() {
-            "+" => {let operation =  Action::Addition;
-            inter_action = operation},
-            "*" => {let operation= Action::Multiplication;
-            inter_action = operation},
-            _ => {
-                panic!("Invalid operator. Please enter + or *.");
-            }
-          }
-match inter_action {
-    Action::Addition=>{let ans = a + b; 
-    final_ans = ans}
-    Action::Multiplication => { let ans = a* b;
-    final_ans = ans},
-    Action::None =>{
-      println!("Invalid operator. Please enter + or *.");
-    }
-
-}
-final_ans = final_ans % mod_num;
-println!("{}", final_ans);
-final_ans
-
-
-
-
+   return Some(generators);
 }
 
 
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn test_multiplication() {
-        let modular_arithmetic=remainder_class_algorithm(1, 5, String::from("*"), 5);
-        assert_eq!(modular_arithmetic,0);
+
+
+
+
+
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn test_multiplication() {
+//         let modular_arithmetic=remainder_class_algorithm(1, 5, String::from("*"), 5);
+//         assert_eq!(modular_arithmetic,0);
   
-    }
-    #[test]
-    fn test_addition() {
-        let modular_arithmetic=remainder_class_algorithm(1, 5, String::from("+"), 5);
-        assert_eq!(modular_arithmetic, 1);
+//     }
+//     #[test]
+//     fn test_addition() {
+//         let modular_arithmetic=remainder_class_algorithm(1, 5, String::from("+"), 5);
+//         assert_eq!(modular_arithmetic, 1);
   
-    }
-    #[test]
-    #[should_panic(expected = "Invalid operator. Please enter + or *.")]
-    fn test_invalid_operator() {
-    remainder_class_algorithm(1, 5, String::from(""), 5);
-    }
-}
+//     }
+//     #[test]
+//     #[should_panic(expected = "Invalid operator. Please enter + or *.")]
+//     fn test_invalid_operator() {
+//     remainder_class_algorithm(1, 5, String::from(""), 5);
+//     }
+// }
